421cd58f7dcc39d5500241eb0313933f7f0234cb
unknown <leifachristian@gmail.com>
2024-02-08 14:12:37 -0700

    ok

--- main.js
+++ main.js
@@ -1,45 +1,40 @@
-// main.js
-
-// Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
-const path = require('node:path')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = true // Add this line
 
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: false,
+        }
+      });
+      
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+//   const startUrl = isDev
+//     ? 'http://localhost:3000' // Load directly from the dev server
+//     : url.format({
+//         pathname: path.join(__dirname, './public/index.html'),
+//         protocol: 'file:',
+//         slashes: true,
+//       });
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-}
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
+  mainWindow.loadURL('http://localhost:3000');
 
-  app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+};
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+app.whenReady().then(createWindow);
+
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== 'darwin') app.quit();
+});
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
